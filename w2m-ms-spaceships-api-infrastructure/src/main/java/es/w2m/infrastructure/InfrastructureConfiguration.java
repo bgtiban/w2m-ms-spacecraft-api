0c9fd1db141beebae5ba624ee342c77d27d5a7bf
bgtiban <bgtiban@outlook.es>
2024-11-07 23:08:13 +0100

    organize imports Ok

--- w2m-ms-spaceships-api-infrastructure/src/main/java/es/w2m/infrastructure/InfrastructureConfiguration.java
+++ w2m-ms-spaceships-api-infrastructure/src/main/java/es/w2m/infrastructure/InfrastructureConfiguration.java
@@ -3,7 +3,6 @@ package es.w2m.infrastructure;
 import es.w2m.domain.ISpaceshipRepositoryPort;
 import es.w2m.infrastructure.jpa.ISpaceShipPageableMapperInfra;
 import es.w2m.infrastructure.jpa.ISpaceshipRepository;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
